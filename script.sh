126146a7d321dae9b6a3b05fc0a1274029ae7961
Alexandru Adam <alex@eg312.com>
2012-06-16 16:37:40 +0300

    v1.0

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rootfs=distro
+rootiso=iso
+initramfs=xyz.gz
+
+cd $rootfs
+find . | cpio -o -H newc | gzip -9 > ../$initramfs
+
+cd ..
+cp $initramfs $rootiso
+cd $rootiso
+
+genisoimage -r -V "xyz" -cache-inodes -J -l -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../xyz.iso .
+
+rm ../$initramfs
